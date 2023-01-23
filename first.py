70791a191c01d9f15d3581970416ad69afbe0c8d
kkdor <krdorosz@gmail.com>
2023-01-23 20:49:17 +0100

    Added dynamic parameters for different datasets as well as plot descriptions

--- first.py
+++ first.py
@@ -5,6 +5,13 @@ from sklearn.datasets import make_blobs, make_circles, make_moons
 from sklearn.cluster import KMeans, MeanShift, DBSCAN, estimate_bandwidth
 from sklearn import metrics
 
+default_settings = {
+    "quantile": 0.3,
+    "eps": 0.3,
+    "dataset_name": "",
+    "x_axis": "x coordinates",
+    "y_axis": "y coordinates"
+}
 
 n_clusters = 3
 n_samples = 500
@@ -18,11 +25,43 @@ noisy_moons, _ = make_moons(n_samples=n_samples, noise=0.05)
 df = pd.read_csv('wine-clustering.csv')
 wines = df[['Alcohol', 'Color_Intensity']].to_numpy()
 
-datasets = [blobs, noisy_circles, noisy_moons, wines]
+datasets = [
+    (blobs,
+     {
+         "quantile": 0.2,
+         "eps": 0.5,
+         "dataset_name": "artificially constructed clusters"
+     }),
+    (noisy_circles,
+     {
+         "quantile": 0.2,
+         "eps": 0.2,
+         "dataset_name": "circles with noise"
+     }),
+    (noisy_moons,
+     {
+         "quantile": 0.2,
+         "eps": 0.2,
+         "dataset_name": "moons with noise"
+     }),
+    (wines,
+     {
+         "quantile": 0.2,
+         "eps": 0.4,
+         "dataset_name": "wines dataset",
+         "x_axis": "Alcohol",
+         "y_axis": "Color_Intensity"
+     })
+]
 
-for dataset in datasets:
+for (dataset, algo_params) in datasets:
+    params = default_settings.copy()
+    params.update(algo_params)
+
     fig, axs = plt.subplots(2, 2)
+    fig.suptitle(f"Results for {params['dataset_name']}")
     axs[0, 0].scatter(dataset[:, 0], dataset[:, 1])
+    axs[0, 0].set(title='Raw data', xlabel=params['x_axis'], ylabel=params['y_axis'])
     validation = {'method': ['kmeans', 'meanshift', 'dbscan'], 'Silhouette Coefficient': [],
                   'Calinski-Harabasz Index': [], 'Davies-Bouldin Index': []}
 
@@ -30,24 +69,29 @@ for dataset in datasets:
     kmeans.fit_predict(dataset)
     axs[0, 1].scatter(dataset[:, 0], dataset[:, 1], c=kmeans.labels_)
     axs[0, 1].scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s=50, c='red', alpha=.5)
+    axs[0, 1].set(title='Data processed with KMeans algorithm', xlabel=params['x_axis'], ylabel=params['y_axis'])
     validation['Silhouette Coefficient'].append(metrics.silhouette_score(dataset, kmeans.labels_, metric='euclidean'))
     validation['Calinski-Harabasz Index'].append(metrics.calinski_harabasz_score(dataset, kmeans.labels_))
     validation['Davies-Bouldin Index'].append(metrics.davies_bouldin_score(dataset, kmeans.labels_))
 
-    bandwidth = estimate_bandwidth(dataset, quantile=.2)
+    bandwidth = estimate_bandwidth(dataset, quantile=params['quantile'])
     meanshift = MeanShift(bandwidth=bandwidth)
     meanshift.fit_predict(dataset)
     axs[1, 0].scatter(dataset[:, 0], dataset[:, 1], c=meanshift.labels_)
-    validation['Silhouette Coefficient'].append(metrics.silhouette_score(dataset, meanshift.labels_, metric='euclidean'))
+    axs[1, 0].set(title='Data processed with MeanShift algorithm', xlabel=params['x_axis'], ylabel=params['y_axis'])
+    validation['Silhouette Coefficient'].append(
+        metrics.silhouette_score(dataset, meanshift.labels_, metric='euclidean'))
     validation['Calinski-Harabasz Index'].append(metrics.calinski_harabasz_score(dataset, meanshift.labels_))
     validation['Davies-Bouldin Index'].append(metrics.davies_bouldin_score(dataset, meanshift.labels_))
 
-    dbscan = DBSCAN(eps=.2)
+    dbscan = DBSCAN(eps=params['eps'])
     dbscan.fit_predict(dataset)
     axs[1, 1].scatter(dataset[:, 0], dataset[:, 1], c=dbscan.labels_)
+    axs[1, 1].set(title='Data processed with DBSCAN algorithm', xlabel=params['x_axis'], ylabel=params['y_axis'])
     validation['Silhouette Coefficient'].append(metrics.silhouette_score(dataset, dbscan.labels_, metric='euclidean'))
     validation['Calinski-Harabasz Index'].append(metrics.calinski_harabasz_score(dataset, dbscan.labels_))
     validation['Davies-Bouldin Index'].append(metrics.davies_bouldin_score(dataset, dbscan.labels_))
 
+    print("\n", params['dataset_name'])
     print(pd.DataFrame(data=validation).to_string())
     plt.show()
